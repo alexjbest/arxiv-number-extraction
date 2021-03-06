d69648c51dda15359c32c933fa2b00a2adb1250b
Alex J. Best <alex.j.best@gmail.com>
2021-03-05 19:28:32 -0500

    tweaking for readability

--- open.py
+++ open.py
@@ -7,14 +7,16 @@ DIR = "9902"
 for filename in os.listdir(os.getcwd() +"/" + DIR):
     if filename[:4] != "math" or filename[-3:] != ".gz":
         continue
+    printed = False
     with gzip.open(DIR + "/" + filename) as ar:
-
         for line in ar:
             try:
                 x = re.search("([0-9]+\.)[0-9]{9,}", line.decode("utf-8"))
                 if x:
-                    print(filename)
-                    print(line)
-                    print(x.group(0))
+                    if not printed:
+                        print(filename)
+                        printed = True
+                    print("    " + line.decode("utf-8").strip())
+                    print("    " + x.group(0))
             except UnicodeDecodeError:
                 pass
